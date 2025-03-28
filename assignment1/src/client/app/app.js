435b3ecf45ba0aa08fa8425725b3bf64be612fad
Balamurugan183 <156446238+Balamurugan183@users.noreply.github.com>
2025-03-28 16:48:00 -0400

    Assignment3
    Assignment3

--- assignment1/src/client/app/app.js
+++
@@ -1,7 +0,0 @@
-// Name: Balamurugan Santhosam
-// Course: INFT 2202
-// Date: 2024-03-16
-
-
-// Set the current year in the footer
-document.getElementById('copyrightYear').textContent = new Date().getFullYear();
